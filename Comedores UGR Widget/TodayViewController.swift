db66d6b31bb89a6547305f94bca8fd69749f2ac2
Daniel Barros <dani.barros@me.com>
2016-03-25 17:53:01 +0100

    Added Notification Center widget

---
+++ Comedores UGR Widget/TodayViewController.swift
@@ -0,0 +1,64 @@
+//
+//  TodayViewController.swift
+//  Comedores UGR Widget
+//
+//  Created by Daniel Barros López on 3/25/16.
+//  Copyright © 2016 Daniel Barros. All rights reserved.
+//
+
+import UIKit
+import NotificationCenter
+
+// TODO: Improve UI
+// TODO: Show menu for current day
+class TodayViewController: UIViewController, NCWidgetProviding {
+        
+    @IBOutlet weak var label: UILabel!
+    
+    let fetcher = WeekMenuFetcher()
+    
+    var weekMenu: [DayMenu] = {
+        if let archivedMenu = NSUserDefaults.standardUserDefaults().dataForKey("weekMenu"),
+            menu = NSKeyedUnarchiver.unarchiveObjectWithData(archivedMenu) as? [DayMenu] {
+            return menu
+        } else {
+            return [DayMenu]()
+        }
+        }() {
+        didSet {
+            let archivedMenu = NSKeyedArchiver.archivedDataWithRootObject(weekMenu)
+            NSUserDefaults.standardUserDefaults().setObject(archivedMenu, forKey: "weekMenu")
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateUI()
+    }
+    
+    
+    private func updateUI() {
+        label.text = weekMenu.first?.dishes.reduce("", combine: { $0 + $1 + "\n" }) ?? "No Data"
+    }
+    
+    
+    func widgetPerformUpdateWithCompletionHandler(completionHandler: ((NCUpdateResult) -> Void)) {
+        
+        fetcher.fetchMenuSync(completionHandler: { menu in
+            self.weekMenu = menu
+            self.updateUI()
+            completionHandler(.NewData)
+        }, errorHandler: { error in
+            // TODO: Handle error
+            print(error)
+            completionHandler(.Failed)
+        })
+        
+        // TODO: If there's no update required, use NCUpdateResult.NoData
+    }
+    
+    
+//    func widgetMarginInsetsForProposedMarginInsets(defaultMarginInsets: UIEdgeInsets) -> UIEdgeInsets {
+//        
+//    }
+}
