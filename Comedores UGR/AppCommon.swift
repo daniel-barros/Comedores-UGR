3c70b925f4878b5d1fa7713f0e422d939d663f00
Daniel Barros <dani.barros@me.com>
2016-03-30 11:14:27 +0200

    Text alignment improvement

--- Comedores UGR/AppCommon.swift
+++ Comedores UGR/AppCommon.swift
@@ -1,5 +1,5 @@
 //
-//  UIColor+AppSpecific.swift
+//  AppCommon.swift
 //  Comedores UGR
 //
 //  Created by Daniel Barros López on 3/27/16.
@@ -11,5 +11,12 @@ import UIKit
 extension UIColor {
     class func customRedColor() -> UIColor {
         return UIColor(red: 0.996, green: 0.230, blue: 0.187, alpha: 1)
+    }
+}
+
+
+extension CGSize {
+    static var max: CGSize {
+        return CGSize(width: CGFloat.max, height: CGFloat.max)
     }
 }
--- Comedores UGR/MenuTableViewCell.swift
+++ Comedores UGR/MenuTableViewCell.swift
@@ -17,11 +17,17 @@ class MenuTableViewCell: UITableViewCell {
     @IBOutlet weak var dishLabel2: UILabel!
     @IBOutlet weak var dishLabel3: UILabel!
     
+    @IBOutlet weak var dayNumberTopConstraint: NSLayoutConstraint!
+    @IBOutlet weak var dayNumberCenterConstraint: NSLayoutConstraint!
+    @IBOutlet weak var dayNumberBottomConstraint: NSLayoutConstraint!
+    
     private var dateLabels: [UILabel!] {
         return [monthLabel, dayNumberLabel, dayNameLabel]
     }
     
+    
     func configure(menu menu: DayMenu) {
+        // Text
         monthLabel.text = menu.month
         dayNumberLabel.text = menu.dayNumber
         dayNameLabel.text = menu.dayName
@@ -29,6 +35,7 @@ class MenuTableViewCell: UITableViewCell {
         dishLabel2.text = menu.dishes.second
         dishLabel3.text = menu.dishes.third
         
+        // Today's date highlight
         if menu.isTodayMenu {
             dateLabels.forEach {
                 $0.textColor = UIColor.customRedColor()
@@ -42,6 +49,39 @@ class MenuTableViewCell: UITableViewCell {
                 $0.font = UIFont.systemFontOfSize($0.font.pointSize, weight: fontWeight)
             }
         }
+    }
+    
+    
+    override func layoutSubviews() {
+        // Text alignment
+        switch (dishLabelHasOnlyOneLine(dishLabel1), dishLabelHasOnlyOneLine(dishLabel2), dishLabelHasOnlyOneLine(dishLabel3)) {
+        case (false, true, true):
+            dayNumberTopConstraint.active = false
+            dayNumberCenterConstraint.active = false
+            dayNumberBottomConstraint.active = true
+        case (true, true, false):
+            dayNumberTopConstraint.constant = -(dayNumberLabel.font.lineHeight - dayNumberLabel.font.pointSize) / 2 - (dishLabel2.font.lineHeight - dishLabel2.font.pointSize) / 2
+            dayNumberTopConstraint.active = true
+            dayNumberCenterConstraint.active = false
+            dayNumberBottomConstraint.active = false
+        default:
+            dayNumberTopConstraint.active = false
+            dayNumberCenterConstraint.active = true
+            dayNumberBottomConstraint.active = false
+        }
+        
+        super.layoutSubviews()
+    }
+
+    
+    private func dishLabelHasOnlyOneLine(label: UILabel) -> Bool {
+        if let text = label.text {
+            let oneLineSize = (text as NSString).boundingRectWithSize(CGSize.max, options: [], attributes: [NSFontAttributeName: label.font], context: nil)
+            if oneLineSize.width > label.frame.width {
+                return false
+            }
+        }
+        return true
     }
 }
 
