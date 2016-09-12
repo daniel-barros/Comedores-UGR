5c70c08303636bbece7feb732cd3f73b33747f67
Daniel Barros <dani.barros@me.com>
2016-09-12 17:07:24 +0200

    Revert "Revert "Watch fetcher bug fix related to WCSession""
    This reverts commit 7521ee41e24a956e5e63d1774154df82e792b024.

--- Comedores UGR Watch Extension/MenuManager.swift
+++ Comedores UGR Watch Extension/MenuManager.swift
@@ -52,7 +52,6 @@ class MenuManager: NSObject, WCSessionDelegate {
         session.delegate = self
         session.activateSession()
         self.handler = handler
-        session.sendMessage([:], replyHandler: nil, errorHandler: nil)
     }
     
     
@@ -88,6 +87,8 @@ class MenuManager: NSObject, WCSessionDelegate {
     
     
     func session(session: WCSession, activationDidCompleteWithState activationState: WCSessionActivationState, error: NSError?) {
-        
+        if activationState == .Activated {
+            session.sendMessage([:], replyHandler: nil, errorHandler: nil)
+        }
     }
 }
